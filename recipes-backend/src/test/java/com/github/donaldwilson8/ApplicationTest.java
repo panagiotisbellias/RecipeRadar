cd25ad14ccd13b39902291fd7d2c44584a1aa95b
Panagiotis Bellias <belliaspan@gmail.com>
2024-04-25 11:09:44 +0300

    [#21] Add unit tests for main class and cleanup

--- recipes-backend/src/test/java/com/github/donaldwilson8/ApplicationTest.java
+++ recipes-backend/src/test/java/com/github/donaldwilson8/ApplicationTest.java
@@ -1,8 +1,17 @@
 package com.github.donaldwilson8;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 class ApplicationTest {
+
+    @Test
+    void testMain() {
+        Application.main(new String[]{});
+        Assertions.assertTrue(true);
+    }
+
 }
