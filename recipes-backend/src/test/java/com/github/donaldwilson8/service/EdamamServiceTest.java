a9d1c2008445c0d185633bc219695818e7e4460b
Panagiotis Bellias <belliaspan@gmail.com>
2024-04-25 11:09:18 +0300

    [#21] Add unit tests for service package

--- recipes-backend/src/test/java/com/github/donaldwilson8/service/EdamamServiceTest.java
+++ recipes-backend/src/test/java/com/github/donaldwilson8/service/EdamamServiceTest.java
@@ -1,8 +1,55 @@
 package com.github.donaldwilson8.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.donaldwilson8.exception.CustomExceptions;
+import com.github.donaldwilson8.model.Hit;
+import com.github.donaldwilson8.model.Recipe;
+import com.github.donaldwilson8.model.RecipeSearchResponse;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.List;
 
 @ExtendWith(MockitoExtension.class)
 class EdamamServiceTest {
+
+    @InjectMocks
+    EdamamService edamamService;
+
+    @Mock
+    WebClient webClient;
+
+    @Mock
+    ObjectMapper objectMapper;
+
+    @Test
+    void testConstructor() {
+        EdamamService edamamService = new EdamamService(objectMapper);
+        Assertions.assertInstanceOf(EdamamService.class, edamamService);
+    }
+
+    @Test
+    void testSearchRecipesException() {
+        CustomExceptions.EdamamApiException exception = Assertions.assertThrows(CustomExceptions.EdamamApiException.class, () -> edamamService.searchRecipes("query"));
+        Assertions.assertEquals("Error response from Edamam API: 401 UNAUTHORIZED; org.springframework.web.reactive.function.client.DefaultClientResponse", exception.getMessage().split("@")[0]);
+    }
+
+    @Test
+    void testParseResponseToRecipes() {
+        RecipeSearchResponse response = Mockito.mock(RecipeSearchResponse.class);
+        Hit hit = Mockito.mock(Hit.class);
+
+        Mockito.when(response.getHits()).thenReturn(List.of(hit));
+        List<Recipe> recipes = edamamService.parseResponseToRecipes(response);
+
+        Assertions.assertEquals(1, recipes.size());
+        Assertions.assertNull(recipes.get(0));
+    }
+
 }
--- recipes-backend/src/test/java/com/github/donaldwilson8/service/UserServiceTest.java
+++ recipes-backend/src/test/java/com/github/donaldwilson8/service/UserServiceTest.java
@@ -1,8 +1,99 @@
 package com.github.donaldwilson8.service;
 
+import com.github.donaldwilson8.exception.CustomExceptions;
+import com.github.donaldwilson8.model.User;
+import com.github.donaldwilson8.repository.UserRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
 
 @ExtendWith(MockitoExtension.class)
 class UserServiceTest {
+
+    @InjectMocks
+    UserService userService;
+
+    @Mock
+    UserRepository userRepository;
+
+    @Mock
+    User user;
+
+    @Test
+    void testConstructor() {
+        UserService userService = new UserService(userRepository);
+        Assertions.assertInstanceOf(UserService.class, userService);
+    }
+
+    @Test
+    void testGetAllUsers() {
+        Assertions.assertTrue(userService.getAllUsers().isEmpty());
+    }
+
+    @Test
+    void testGetUserById() {
+        Assertions.assertNull(userService.getUserById("id"));
+    }
+
+    @Test
+    void testGetUserByUsername() {
+        Assertions.assertNull(userService.getUserByUsername("username"));
+    }
+
+    @Test
+    void testCreateUser() {
+        Assertions.assertNull(userService.createUser(user));
+    }
+
+    @Test
+    void testCreateUserExistingUsername() {
+        Mockito.when(user.getUsername()).thenReturn("username");
+        Mockito.when(userRepository.existsByUsername("username")).thenReturn(true);
+
+        CustomExceptions.DuplicateUserException exception = Assertions.assertThrows(CustomExceptions.DuplicateUserException.class, () -> userService.createUser(user));
+        Assertions.assertEquals("Username is already taken.", exception.getMessage());
+    }
+
+    @Test
+    void testCreateUserExistingEmail() {
+        Mockito.when(user.getEmail()).thenReturn("email");
+        Mockito.when(userRepository.existsByEmail("email")).thenReturn(true);
+
+        CustomExceptions.DuplicateUserException exception = Assertions.assertThrows(CustomExceptions.DuplicateUserException.class, () -> userService.createUser(user));
+        Assertions.assertEquals("Email is already in use.", exception.getMessage());
+    }
+
+    @Test
+    void testUpdateUser() {
+        Mockito.when(user.get_id()).thenReturn("id");
+        Mockito.when(user.getAge()).thenReturn(1);
+        Mockito.when(user.getEmail()).thenReturn("email");
+        Mockito.when(user.getGender()).thenReturn("gender");
+        Mockito.when(user.getName()).thenReturn("name");
+        Mockito.when(user.getPassword()).thenReturn("password");
+        Mockito.when(user.getPhone()).thenReturn("phone");
+        Mockito.when(user.getDietaryRestrictions()).thenReturn(new String[]{});
+        Mockito.when(userRepository.findById("id")).thenReturn(Optional.of(user));
+        Assertions.assertNull(userService.updateUser(user));
+    }
+
+    @Test
+    void testUpdateUserNull() {
+        Mockito.when(user.get_id()).thenReturn("id");
+        Mockito.when(userRepository.findById("id")).thenReturn(Optional.of(user));
+        Assertions.assertNull(userService.updateUser(user));
+    }
+
+    @Test
+    void testUpdateUserAbsent() {
+        CustomExceptions.UserNotFoundException exception = Assertions.assertThrows(CustomExceptions.UserNotFoundException.class, () -> userService.updateUser(user));
+        Assertions.assertEquals("User not found with id: null", exception.getMessage());
+    }
+
 }
